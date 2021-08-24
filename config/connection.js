8a92692473fdefd039af25b7b01ecaa1a2caf021
Shane <shane.messmer1@gmail.com>
2021-08-24 18:06:03 -0400

    changes port

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,8 @@
 const Sequelize = require('sequelize');
+const PORT = process.env.PORT || 3001;
+
+
+
 
 require('dotenv').config();
 
@@ -11,7 +15,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
+    port: PORT
   });
 }
 module.exports = sequelize;
